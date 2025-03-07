039e0fc7301c52d6d36d2a9c0321556dbf56c72d
rakib12212 <rakibulislamr83@gmail.com>
2025-03-08 03:10:15 +0600

    complete responsive v2

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import assets from "@/assets";
 import { FiMenu, FiX } from "react-icons/fi";
+import Link from "next/link";
 
 const Navbar = ({ toggleSidebar, isSidebarOpen }) => {
   return (
@@ -17,33 +18,35 @@ const Navbar = ({ toggleSidebar, isSidebarOpen }) => {
         </button>
 
         {/* Center the logo and text */}
-        <div className="flex items-center gap-x-4">
-          {assets.images.logo && (
-            <Image
-              src={assets.images.logo}
-              width={40}
-              height={40}
-              alt="logo"
-              className="self-center"
-            />
-          )}
-          <div className="relative flex flex-col pb-4">
-            <h1 className="font-bold text-2xl tracking-wide text-[#713CF4]">
-              EchoGPT
-            </h1>
-            <h1
-              className="absolute font-bold text-2xl tracking-wide left-0 right-0 top-[0.9em] scale-y-[-1] opacity-30"
-              style={{
-                maskImage:
-                  "linear-gradient(to bottom, rgba(100, 80, 244, 0.5), transparent)",
-                WebkitMaskImage:
-                  "linear-gradient(to top, rgba(100, 80, 244, 0.5), transparent)",
-              }}
-            >
-              EchoGPT
-            </h1>
+        <Link href="/">
+          <div className="flex items-center gap-x-4">
+            {assets.images.logo && (
+              <Image
+                src={assets.images.logo}
+                width={40}
+                height={40}
+                alt="logo"
+                className="self-center"
+              />
+            )}
+            <div className="relative flex flex-col pb-4">
+              <h1 className="font-bold text-2xl tracking-wide text-[#713CF4]">
+                EchoGPT
+              </h1>
+              <h1
+                className="absolute font-bold text-2xl tracking-wide left-0 right-0 top-[0.9em] scale-y-[-1] opacity-30"
+                style={{
+                  maskImage:
+                    "linear-gradient(to bottom, rgba(100, 80, 244, 0.5), transparent)",
+                  WebkitMaskImage:
+                    "linear-gradient(to top, rgba(100, 80, 244, 0.5), transparent)",
+                }}
+              >
+                EchoGPT
+              </h1>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
 
       {/* Ensure the user image is centered */}
